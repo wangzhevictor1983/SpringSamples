e6a5cb402bd1639de947ef39b314b38dc4d2f71f
Victor <wangzhevictor1983@outlook.com>
2019-04-26 00:04:42 +0800

    update

--- springbasicdemo/demo/src/main/java/com/zhe/springbasic/demo/App.java
+++ springbasicdemo/demo/src/main/java/com/zhe/springbasic/demo/App.java
@@ -1,5 +1,7 @@
 package com.zhe.springbasic.demo;
 
+import org.springframework.context.ApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	ApplicationContext context;
         System.out.println( "Hello World!" );
     }
 }
